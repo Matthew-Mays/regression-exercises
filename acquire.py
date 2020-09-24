357bf484a60f52cbf339698637465c806fce685b
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-09-24 16:59:13 -0500

    modified acquire and prepare python

--- acquire.py
+++ acquire.py
@@ -55,3 +55,39 @@ def get_iris_data():
 # If they exist, use the .csv file. If the file doesn't exist, then produce the SQL and pandas necessary 
 # to create a dataframe, then write the dataframe to a .csv file with the appropriate name.
 
+
+#################################    Acquire Mall Customers Data     ################################################
+
+def new_mall_data():
+    '''
+    This function reads the mall data from the Codeup db 
+    into a df, write it to a csv and returns df.
+    '''
+    sql_query = 'SELECT * FROM customers'
+    df = pd.read_sql(sql_query, get_connection('mall_customers'))
+    df.to_csv('mall_customers_df.csv')
+    return df
+
+def get_mall_data(cached=False):
+    '''
+    This function reads in mall customer data from Codeup db if cached == False
+    or if cached == True reads in mall customer df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('mall_customers_df.csv') == False:
+        df = new_mall_data()
+    else:
+        df = pd.read_csv('mall_customers_df.csv', index_col=0)
+    return df
+
+
+def get_telco_data():
+    filename = 'telco.csv'
+    if os.path.isfile(filename) == False:
+        df = pd.read_sql('''SELECT * FROM customers JOIN internet_service_types 
+                        USING(internet_service_type_id)
+                        JOIN contract_types USING(contract_type_id)
+                        JOIN payment_types USING(payment_type_id)''', get_connection('telco_churn'))
+        df.to_csv(filename)
+    else:
+        df = pd.read_csv(filename, index_col=0)
+    return df
--- prepare.py
+++ prepare.py
@@ -87,15 +87,40 @@ def prep_titanic():
     return train, validate, test
 
 
-# In[88]:
+
+def prep_mall_data(df):
+    '''
+    Takes the acquired mall data, does data prep, and returns
+    train, validate, and test data splits
+    '''
+    df['is_female'] = (df.gender == 'Female').astype('int')
+    train_validate, test = train_test_split(df, test_size=.15, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.15, random_state=123)
+    return train, validate, test
 
 
-train, validate, test = prep_titanic()
-validate
-
-
-# In[ ]:
-
-
-
-
+def prep_telco_data(df):
+    # Drop duplicates if there are any
+    df.drop_duplicates(inplace=True)
+    #
+    payment_type_cols = [1, 2, 3, 4]
+    df['is_automatic_payment'] = df.payment_type_id.replace(payment_type_cols, [0,0,1,1])
+    no_internet = ['streaming_movies', 'tech_support', 'streaming_tv', 'online_security', 'online_backup', 'device_protection']
+    df[no_internet] = df[no_internet].replace({'No internet service': 0, 'No': 1, 'Yes': 2})
+    binary_cols = ['churn', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'paperless_billing']
+    df[binary_cols] = df[binary_cols].replace('Yes', 1).replace('No', 0)
+    df.rename(columns={'gender':'male'}, inplace=True)
+    df['male'] = df['male'].replace('Male', 1).replace('Female', 0)
+    df['total_charges'] = df.total_charges.where((df.tenure != 0), 0)
+    df = df.astype({'total_charges':'float64'})
+    df['phone_lines'] = df['multiple_lines'].replace({'No phone service': 0, 'No': 1, 'Yes': 2})
+    df.drop(columns='multiple_lines')
+    df.rename(columns={'tenure':'monthly_tenure'}, inplace=True)
+    df['yearly_tenure'] = round(df.monthly_tenure / 12, 2)
+    df['part_depend'] = df['partner'] + df['dependents']
+    df = df.drop(df[((df['monthly_tenure'].sort_values() == 0))].index)
+    internet_types = [1, 2, 3]
+    df['fiber'] = df['internet_service_type_id'].replace(internet_types, [0, 1, 0])
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    return train, validate, test
